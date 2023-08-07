be1b2faf26e744709deff7f66749fdde0c167350
qasim munir <writingtoqasim@gmail.com>
2023-08-07 15:22:02 +0500

    commiting to github repo qasim-nodejs from local machine

---
+++ scripts/application_start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# Example: Start the Node.js application using npm
+cd /home/ec2-user/hellojs
+npm start
+
---
+++ scripts/before_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Example: Install any required dependencies or perform pre-installation tasks
+echo "Running pre-installation tasks..."
+# Add your pre-installation commands here
